f5146f3473a9772d5ee03397275b077a5db89ce6
johnwhyyy <johnyangqiang@gmail.com>
2026-07-13 15:11:36 +0800

    TEST: deploy on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/qs_global_university_map/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "github-pages" ? "/qs_global_university_map/" : "/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 3000,
     target: "es2020",
     sourcemap: true
   }
-});
+}));
